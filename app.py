b9a0800e5621efda08979d58e9a50c8797ba8c41
aamir <2019aamir.ansari@ves.ac.in>
2021-10-01 13:13:52 +0530

    added description of app and decorator in main

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+# * Flask constructor takes the name of current module (__name__) as argument.
 app = Flask(__name__)
 
 # ? *** Base route ***
+# * The route() function of the Flask class is a decorator 
+# * it tells the application which URL should call the associated function.
 @app.route('/')
 def home():
     return "Hello there!"
